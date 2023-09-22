92cd0b97c20765a7672cd7864cc309ddb6a2d329
ivandjoh <idsta.ivandi@xl.co.id>
2023-09-23 00:44:02 +0700

    generate dynamic metadata

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,5 +1,8 @@
 import getUser from "@/lib/getUser";
 import getUserPosts from "@/lib/getUserPosts";
+import {Suspense} from "react";
+import UserPosts from "@/app/users/[userId]/components/UserPosts";
+import {Metadata} from "next";
 
 
 type Params = {
@@ -8,11 +11,34 @@ type Params = {
     }
 }
 
-export default async function userPage({params: {userId}}: Params) {
-const userData: Promise<User> = getUser(userId);
-const userPostsData: Promise<Post[]> = getUserPosts(userId);
+export async function generateMetadata(
+    {
+        params: {userId}
+    }: Params): Promise<Metadata> {
+    const userData: Promise<User> = getUser(userId);
+    const user = await userData;
+    return {
+        title: user.name,
+        description: user.email
+    }
+}
 
+export default async function userPage({params: {userId}}: Params) {
+    const userData: Promise<User> = getUser(userId);
+    const userPostsData: Promise<Post[]> = getUserPosts(userId);
+
+    // const [user, userPosts] = await Promise.all([userData, userPostsData]);
+
+    const user = await userData;
     return (
-        <></>
+        <>
+            <h1>{user.name}</h1>
+            <br/>
+            <Suspense fallback={<div>Loading...</div>}>
+                <UserPosts promise={userPostsData}/>
+            </Suspense>
+        </>
     );
+
+
 }
